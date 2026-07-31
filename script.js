254cf345039fcebcd5701a33e517781db029b4bd
Benjamin <benjag1150@gmail.com>
2026-07-31 17:49:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,100 @@
-<script>
-tsParticles.load("tsparticles",{
+```javascript id="olqvtq"
+tsParticles.load("tsparticles", {
 
-background:{
-    color:"#000000"
-},
-
-fpsLimit:120,
-
-particles:{
-
-    number:{
-        value:120
-    },
-
-    color:{
-        value:"#8A2BE2"
-    },
-
-    links:{
-        enable:true,
-        distance:150,
-        color:"#8A2BE2",
-        opacity:0.45,
-        width:1
-    },
-
-    move:{
-        enable:true,
-        speed:2
-    },
-
-    opacity:{
-        value:0.6
-    },
-
-    size:{
-        value:{min:2,max:4}
-    }
-
-},
-
-interactivity:{
-
-    events:{
-
-        onHover:{
-            enable:true,
-            mode:["grab","repulse"]
-        },
-
-        onClick:{
-            enable:true,
-            mode:"push"
+    background: {
+        color: {
+            value: "#000000"
         }
-
     },
 
-    modes:{
+    fpsLimit: 120,
 
-        grab:{
-            distance:220,
-            links:{
-                opacity:1
+    particles: {
+
+        number: {
+            value: 120,
+            density: {
+                enable: true,
+                area: 800
             }
         },
 
-        repulse:{
-            distance:120,
-            duration:0.4
+        color: {
+            value: "#8A2BE2"
         },
 
-        push:{
-            quantity:4
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.6
+        },
+
+        size: {
+            value: {
+                min: 2,
+                max: 4
+            }
+        },
+
+        links: {
+            enable: true,
+            distance: 150,
+            color: "#8A2BE2",
+            opacity: 0.45,
+            width: 1
+        },
+
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            outModes: {
+                default: "bounce"
+            }
         }
 
-    }
+    },
 
-},
+    interactivity: {
 
-detectRetina:true
+        events: {
+
+            onHover: {
+                enable: true,
+                mode: "grab"
+            },
+
+            onClick: {
+                enable: true,
+                mode: "push"
+            },
+
+            resize: true
+
+        },
+
+        modes: {
+
+            grab: {
+                distance: 220,
+                links: {
+                    opacity: 1
+                }
+            },
+
+            push: {
+                quantity: 4
+            }
+
+        }
+
+    },
+
+    detectRetina: true
 
 });
-</script>
+```
