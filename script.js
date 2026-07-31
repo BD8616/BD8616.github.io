e83ef8328a4171e90b201e422fa653cf2b07eb9a
Benjamin <benjag1150@gmail.com>
2026-07-31 17:44:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,84 @@
-tsParticles.load("tsparticles", {
-    background: {
-        color: "#000000"
+<script>
+tsParticles.load("tsparticles",{
+
+background:{
+    color:"#000000"
+},
+
+fpsLimit:120,
+
+particles:{
+
+    number:{
+        value:120
     },
 
-    fpsLimit: 120,
+    color:{
+        value:"#8A2BE2"
+    },
 
-    particles: {
-        number: {
-            value: 120,
-            density: {
-                enable: true,
-                value_area: 800
-            }
+    links:{
+        enable:true,
+        distance:150,
+        color:"#8A2BE2",
+        opacity:0.45,
+        width:1
+    },
+
+    move:{
+        enable:true,
+        speed:2
+    },
+
+    opacity:{
+        value:0.6
+    },
+
+    size:{
+        value:{min:2,max:4}
+    }
+
+},
+
+interactivity:{
+
+    events:{
+
+        onHover:{
+            enable:true,
+            mode:["grab","repulse"]
         },
 
-        color: {
-            value: "#8A2BE2"
-        },
-
-        links: {
-            enable: true,
-            distance: 150,
-            color: "#8A2BE2",
-            opacity: 0.45,
-            width: 1
-        },
-
-        move: {
-            enable: true,
-            speed: 2
-        },
-
-        opacity: {
-            value: 0.6
-        },
-
-        size: {
-            value: {
-                min: 2,
-                max: 4
-            }
+        onClick:{
+            enable:true,
+            mode:"push"
         }
+
     },
 
-    interactivity: {
-        events: {
-            onHover: {
-                enable: true,
-                mode: ["grab", "repulse"]
-            },
+    modes:{
 
-            onClick: {
-                enable: true,
-                mode: "push"
+        grab:{
+            distance:220,
+            links:{
+                opacity:1
             }
         },
 
-        modes: {
-            grab: {
-                distance: 220,
-                links: {
-                    opacity: 1
-                }
-            },
+        repulse:{
+            distance:120,
+            duration:0.4
+        },
 
-            repulse: {
-                distance: 120,
-                duration: 0.4
-            },
-
-            push: {
-                quantity: 4
-            }
+        push:{
+            quantity:4
         }
-    },
 
-    detectRetina: true
+    }
+
+},
+
+detectRetina:true
+
 });
+</script>
